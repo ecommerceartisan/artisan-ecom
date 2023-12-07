ce52d875036b7185718a2ac5ef4a1739bbc00d71
ecommerceartisan <ecommerceartisan93@gmail.com>
2023-12-08 02:14:02 +0800

    front-end for forgot-login-register
    front-end for forgot-login-register

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -1,96 +1,113 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // Axios for making HTTP requests
-import { useNavigate } from 'react-router-dom'; // React Router's useNavigate hook for navigation
-import Layout from '../../components/Layout/Layout'; // Layout component for page structure
-import toast from 'react-hot-toast'; // Toast notifications for user feedback
-import "../../styles/AuthStyle.css"; // Styling for the component
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../../components/Layout/Layout';
+import toast from 'react-hot-toast';
+import "../../styles/AuthStyle.css";
 
 const ForgotPassword = () => {
-  // State variables to store user input
-  const [email, setEmail] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [answer, setAnswer] = useState('');
+    const [email, setEmail] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [selectedQuestion, setSelectedQuestion] = useState('');  // New state for selected security question
+    const [answer, setAnswer] = useState('');
 
-  // React Router's useNavigate hook to programmatically navigate to different routes
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  // Function to handle form submission when resetting the password
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Send a POST request to the '/api/v1/auth/forgot-password' endpoint
-      const res = await axios.post('/api/v1/auth/forgot-password', {
-        email,
-        newPassword,
-        answer,
-      });
-      if (res && res.data.success) {
-        // If the request is successful, show a success message and navigate to the login page
-        toast.success(res.data.message);
-        navigate('/login');
-      } else {
-        // If there's an error, show an error message
-        toast.error(res.data.message);
-      }
-    } catch (error) {
-      console.log(error);
-      // If there's an unexpected error, display a generic error message
-      toast.error('Something went wrong');
-    }
-  };
+    const securityQuestions = [
+        "In what city were you born?",
+        "What is the name of your favorite pet?",
+        "What is your mother's maiden name?",
+        "What high school did you attend?",
+        "What was the name of your elementary school?",
+        "What was the make of your first car?",
+        "What was your favorite food as a child?",
+    ];
 
-  return (
-    // Page layout structure with a title and a form for resetting the password
-    <Layout title="Reset Password - Artisan Ecommerce">
-      <div className="form-container">
-        <h3>Reset Password</h3>
-        <form onSubmit={handleSubmit}>
-          {/* Input field for email */}
-          <div className="mb-3">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail"
-              placeholder="Enter Email Address"
-              required
-            />
-          </div>
-          {/* Input field for secret answer */}
-          <div className="mb-3">
-            <input
-              type="text"
-              value={answer}
-              onChange={(e) => setAnswer(e.target.value)}
-              className="form-control"
-              id="exampleInputAnswer"
-              placeholder="Enter Your Secret Answer"
-              required
-            />
-          </div>
-          {/* Input field for new password */}
-          <div className="mb-3">
-            <input
-              type="password" // Use "password" type for password input
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-              className="form-control"
-              id="exampleInputPassword"
-              placeholder="Enter Password"
-              required
-            />
-          </div>
-          {/* Submit button for password reset */}
-          <div className="mb-3">
-            <button type="submit" className="btn btn-primary">
-              Reset Password
-            </button>
-          </div>
-        </form>
-      </div>
-    </Layout>
-  );
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('/api/v1/auth/forgot-password', {
+                email,
+                newPassword,
+                securityQuestions: [
+                    {
+                        question: selectedQuestion,
+                        answer,
+                    }
+                ],
+            });
+            if (res && res.data.success) {
+                toast.success(res.data.message);
+                navigate('/login');
+            } else {
+                toast.error(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error('Something went wrong');
+        }
+    };
+
+    return (
+        <Layout title="Reset Password - Artisan Ecommerce">
+            <div className="form-container">
+                <h3>Reset Password</h3>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="form-control"
+                            placeholder="Enter Email Address"
+                            required
+                        />
+                    </div>
+                    {/* Dropdown for security questions */}
+                    <div className="mb-3">
+                        <label htmlFor="securityQuestion">Select a Security Question</label>
+                        <select
+                            id="securityQuestion"
+                            className="form-control"
+                            value={selectedQuestion}
+                            onChange={(e) => setSelectedQuestion(e.target.value)}
+                            required
+                        >
+                            <option value="" disabled>Select a question</option>
+                            {securityQuestions.map((question, index) => (
+                                <option key={index} value={question}>{question}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="mb-3">
+                        <input
+                            type="text"
+                            value={answer}
+                            onChange={(e) => setAnswer(e.target.value)}
+                            className="form-control"
+                            placeholder="Enter Your Secret Answer"
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <input
+                            type="password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            className="form-control"
+                            placeholder="Enter Password"
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <button type="submit" className="btn btn-primary">
+                            Reset Password
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </Layout>
+    );
 };
 
 export default ForgotPassword;
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,135 +1,146 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // Axios for making HTTP requests
-import { useNavigate } from "react-router-dom"; // React Router hook for navigation
-import Layout from '../../components/Layout/Layout'; // Layout component for page structure
-import toast from 'react-hot-toast'; // Toast notifications for user feedback
-import "../../styles/AuthStyle.css"; // Styling for the component
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../../components/Layout/Layout';
+import toast from 'react-hot-toast';
+import "../../styles/AuthStyle.css";
 
 const Register = () => {
-    // State variables to store user input
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
     const [address, setAddress] = useState("");
+    const [selectedQuestion, setSelectedQuestion] = useState("");
     const [answer, setAnswer] = useState("");
 
-    // React Router hook for navigation
     const navigate = useNavigate();
 
-    // Function to handle form submission for user registration
+    const securityQuestions = [
+        "In what city were you born?",
+        "What is the name of your favorite pet?",
+        "What is your mother's maiden name?",
+        "What high school did you attend?",
+        "What was the name of your elementary school?",
+        "What was the make of your first car?",
+        "What was your favorite food as a child?",
+    ];
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            // Send a POST request to the '/api/v1/auth/register' endpoint
             const res = await axios.post("/api/v1/auth/register", {
                 name,
                 email,
                 password,
                 phone,
                 address,
-                answer,
+                securityQuestions: [
+                    {
+                        question: selectedQuestion,
+                        answer,
+                    }
+                ],
             });
             if (res && res.data.success) {
-                // If the request is successful, show a success message and navigate to the login page
                 toast.success(res.data && res.data.message);
                 navigate("/login");
             } else {
-                // If there's an error, show an error message
                 toast.error(res.data.message);
             }
         } catch (error) {
             console.log(error);
-            // If there's an unexpected error, display a generic error message
             toast.error("Something went wrong");
         }
     };
 
-    // Page layout structure with a title and a registration form
     return (
         <Layout title={"Registration - Atisan Ecommerce"}>
             <div className="form-container">
                 <h3>REGISTRATION</h3>
                 <form onSubmit={handleSubmit}>
-                    {/* Input field for user's full name */}
                     <div className="mb-3">
                         <input
                             type="text"
                             value={name}
                             onChange={(e) => setName(e.target.value)}
                             className="form-control"
-                            id="exampleInputName"
                             placeholder='Enter Full Name'
                             required
                             autoFocus
                         />
                     </div>
-                    {/* Input field for user's email */}
                     <div className="mb-3">
                         <input
                             type="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             className="form-control"
-                            id="exampleInputEmail"
                             placeholder='Enter Email Address'
                             required
                             autoFocus
                         />
                     </div>
-                    {/* Input field for user's password */}
                     <div className="mb-3">
                         <input
                             type="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             className="form-control"
-                            id="exampleInputPassword"
                             placeholder='Enter Password'
                             required
                             autoFocus
                         />
                     </div>
-                    {/* Input field for user's phone number */}
                     <div className="mb-3">
                         <input
                             type="text"
                             value={phone}
                             onChange={(e) => setPhone(e.target.value)}
                             className="form-control"
-                            id="exampleInputPhone"
                             placeholder='Enter Phone Number'
                             required
                             autoFocus
                         />
                     </div>
-                    {/* Input field for user's full address */}
                     <div className="mb-3">
                         <input
                             type="text"
                             value={address}
                             onChange={(e) => setAddress(e.target.value)}
                             className="form-control"
-                            id="exampleInputAddress"
                             placeholder='Enter Full Address'
                             required
                             autoFocus
                         />
                     </div>
-                    {/* Input field for a secret question answer */}
+                    {/* Dropdown for security questions */}
+                    <div className="mb-3">
+                        <label htmlFor="securityQuestion">Select a Security Question</label>
+                        <select
+                            id="securityQuestion"
+                            className="form-control"
+                            value={selectedQuestion}
+                            onChange={(e) => setSelectedQuestion(e.target.value)}
+                            required
+                        >
+                            <option value="" disabled>Select a question</option>
+                            {securityQuestions.map((question, index) => (
+                                <option key={index} value={question}>{question}</option>
+                            ))}
+                        </select>
+                    </div>
                     <div className="mb-3">
                         <input
                             type="text"
                             value={answer}
                             onChange={(e) => setAnswer(e.target.value)}
                             className="form-control"
-                            id="exampleInputAnswer"
-                            placeholder='What is your Favorite food?'
+                            placeholder='Answer to Security Question'
                             required
                             autoFocus
                         />
                     </div>
-                    {/* Submit button for user registration */}
                     <button type="submit" className="btn btn-primary">Sign Up</button>
                 </form>
             </div>
